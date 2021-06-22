0da5417b7ccd570bde6299d9a64ddf1bbfa58c0c
Mason-Clarke <52585365+Mason-Clarke@users.noreply.github.com>
2021-06-22 17:42:28 -0300

    A runner to perform encoding on a text file

---
+++ Huffman/HuffmanDemo.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+
+/**
+ * CSCI 2110
+ * HuffmanDemo.java
+ * This is a demo class for the purpose of testing the Huffman encode() and decode() methods.
+ * Created by Mason Clarke, March 30, 2021
+ */
+public class HuffmanDemo {
+
+  public static void main (String [] args) throws IOException {
+    Huffman.encode();
+    Huffman.decode();
+  }
+}
